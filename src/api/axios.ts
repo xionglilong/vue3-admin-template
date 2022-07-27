8c9370418abd91537bf65a6c919b7c899374a8ed
tianxinying <tianxinying@byteluck.com>
2022-07-27 11:15:50 +0800

    feat: 上线bug修改

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,6 +4,20 @@ const axiosInstance = axios.create({
   baseURL:import.meta.env.VITE_URL,
 
 })
+console.log(import.meta.env.VITE_URL);
+// request拦截器 获取用户数据的时候，要告诉服务器现在是已经的登录的状态，在header附带token值，验证当前用户
+axiosInstance.interceptors.request.use(
+  (requestInfo) => {
+    if (requestInfo.headers) {
+      requestInfo.headers['token'] = localStorage.getItem('token') || '0'
+      requestInfo.headers['Content-Type'] = 'application/json;charset=UTF-8'
+      return requestInfo
+    }
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 // response 拦截器
 axiosInstance.interceptors.response.use(response=>{
   // debugger
